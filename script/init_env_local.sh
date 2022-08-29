b13b6304816b371b9a4bb9cc7e9c9b77e87c16a5
Navy <navy@126.com>
2022-08-29 17:10:07 +0800

    use cromwell

---
+++ script/init_env_local.sh
@@ -0,0 +1,7 @@
+# This script is to init local environment. source this script in terminal before run DyqPipe.wdl
+#
+# > source init_env_local.sh
+#
+
+export PATH=$(pwd)/cmd:$PATH
+export PYTHONPATH=$(pwd)
